0a37fb8538534f0a7dc2c2cfdd37778b8db3163b
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2020-12-10 16:47:37 +0000

    gp_predict: move prediction to function

--- src/gp_predict.py
+++ src/gp_predict.py
@@ -9,6 +9,28 @@ import gpflow.kernels
 
 from gp_model import build_model, prepare_X, predict_logpmf
 
+def make_predictions(model, model_opts, model_params, dset, nsamples=200):
+    # generate expected logit-hazard rate
+    Xs = prepare_X(dset, model.n_lags, model.max_nt)
+    dset['logit_hazard'] = [model.predict_f(X)[0] for X in Xs]
+
+    # add filtered signal if projected kernel
+    if 'proj' in model_opts['kernels_input']:
+        W = next(v for (k, v) in model_params.items() if k.endswith('/W'))
+        Xs_proj = [X[:, :-3].dot(W) for X in Xs]
+        dset['projected_stim'] = Xs_proj
+
+    # decompose prediction if additive kernel
+    if isinstance(model.kern, gpflow.kernels.Sum):
+        logit_hazard = zip(*[model.predict_f_partial(X) for X in Xs])
+        for hazard, k_input in zip(logit_hazard, model_opts['kernels_input']):
+            dset['logit_hazard_{}'.format(k_input)] = hazard
+
+    # sample predictive distribution
+    if nsamples is not None:
+        dset['log_pmf'] = predict_logpmf(model, dset, nsamples)
+
+    return dset
 
 def main(result_dir, pred_filename, *, nsamples=None):
     """Generate predictions from a fitted GP model for experimental data
@@ -32,26 +54,7 @@ def main(result_dir, pred_filename, *, nsamples=None):
     model_params = dict(np.load(result_path / 'model_params_best.npz'))
     model.assign(model_params)
 
-    # generate expected logit-hazard rate
-    Xs = prepare_X(dset, model.n_lags, model.max_nt)
-    dset['logit_hazard'] = [model.predict_f(X)[0] for X in Xs]
-
-    # add filtered signal if projected kernel
-    if 'proj' in model_opts['kernels_input']:
-        W = next(v for (k, v) in model_params.items() if k.endswith('/W'))
-        Xs_proj = [X[:, :-3].dot(W) for X in Xs]
-        dset['projected_stim'] = Xs_proj
-
-    # decompose prediction if additive kernel
-    if isinstance(model.kern, gpflow.kernels.Sum):
-        logit_hazard = zip(*[model.predict_f_partial(X) for X in Xs])
-        for hazard, k_input in zip(logit_hazard, model_opts['kernels_input']):
-            dset['logit_hazard_{}'.format(k_input)] = hazard
-
-    # sample predictive distribution
-    if nsamples is not None:
-        dset['log_pmf'] = predict_logpmf(model, dset, nsamples)
-
+    dset = make_predictions(model, model_opts, model_params, dset, nsamples)
     # save predictions
     dset.to_pickle(pred_filename)
 
