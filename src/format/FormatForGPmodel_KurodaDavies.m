530ce3acf8a30ee1478b7b851e273eb3b33a8bb4
Sumiya Kuroda <26479377+sumiya-kuroda@users.noreply.github.com>
2024-02-29 17:58:03 +0000

    Update file conversion scripts to support outputs with separate animals

--- src/format/FormatForGPmodel_KurodaDavies.m
+++ src/format/FormatForGPmodel_KurodaDavies.m
@@ -37,6 +37,21 @@ if concat
 
         clear struct
     end
+
+    rt = vertcat([rtAll{:}]);
+    sig = vertcat([sigAll{:}]).';
+    session = vertcat(sessionAll{:});
+    hazard = vertcat([hazardAll{:}]).';
+    outcome = vertcat([outcomeAll{:}]).';
+    change = vertcat([changeAll{:}]).';
+    ys = vertcat([ysAll{:}]);
+    clearvars -except rt sig session hazard outcome ys change species ResultFolder animals
+    
+    if trcmp(species, 'humans')
+        save(fullfile(ResultFolder,'dataAllHumans_GPmodel.mat')); % no '-v7.3'
+    elseif strcmp(species, 'mice')
+        save(fullfile(ResultFolder,'data_AllMiceTraining_GPmodel.mat')); % no '-v7.3'
+    end
 else
     % Run a for loops for all animals
     for a=1:length(animals)
@@ -44,28 +59,37 @@ else
         Data = fsm.fsm(strcmp({fsm.fsm.participant}, animals{a}));
         % List sessions
         sessions = unique({Data.session});
-        disp(["Reading and formatting data for GP model for ", animal_name, "; Number of sessions: ", length(sessions)]);
+        disp(['Reading and formatting data for GP model for ', animal_name, '; Number of sessions: ', int2str(length(sessions))]);
         for s = 1:length(sessions)
-            subData = Data(strcmp({subData.session}, sessions{s}));
+            subData = Data(strcmp({Data.session}, sessions{s}));
+            struct = fsm2GPmodel(subData, sessions{s}, species);
+            outcomeAll{s} = struct.outcome;
+            sigAll{s} = struct.sig;
+            sessionAll{s} = struct.session;
+            hazardAll{s} = struct.hazard;
+            rtAll{s} = struct.rt;
+            changeAll{s} = struct.change;
+            ysAll{s} = struct.ys;
+
+           clear struct
         end
+
+        rt = vertcat([rtAll{:}]);
+        sig = vertcat([sigAll{:}]).';
+        session = vertcat(sessionAll{:});
+        hazard = vertcat([hazardAll{:}]).';
+        outcome = vertcat([outcomeAll{:}]).';
+        change = vertcat([changeAll{:}]).';
+        ys = vertcat([ysAll{:}]);
+        clearvars -except rt sig session hazard outcome ys change ...
+                  species ResultFolder animals animal_name fsm
+
+        if strcmp(species, 'mice')
+            save(fullfile(ResultFolder,['data_MiceTraining_', animal_name, '_GPmodel.mat']), ...
+                "rt","sig","session","hazard","outcome","ys","change"); % no '-v7.3'
+        end
+
     end
-end
-
-rt = vertcat([rtAll{:}]);
-sig = vertcat([sigAll{:}]).';
-session = vertcat(sessionAll{:});
-hazard = vertcat([hazardAll{:}]).';
-outcome = vertcat([outcomeAll{:}]).';
-change = vertcat([changeAll{:}]).';
-ys = vertcat([ysAll{:}]);
-clearvars -except rt sig session hazard outcome ys change species ResultFolder animals concat
-
-if concat && strcmp(species, 'humans')
-    save(fullfile(ResultFolder,'dataAllHumans_GPmodel.mat')); % no '-v7.3'
-elseif concat && strcmp(species, 'mice')
-    save(fullfile(ResultFolder,'data_AllMiceTraining_GPmodel.mat')); % no '-v7.3'
-elseif ~concat && strcmp(species, 'mice')
-    save(fullfile(ResultFolder,['data_All', animals{1}, '_GPmodel.mat'])); % no '-v7.3'
 end
 
 end
